c0b1d1258820b5bd8acdf7c754794de85b0e4199
Kingminer7 <gavin@t8er.com>
2024-09-23 17:05:09 -0400

    just to make render happy

--- bot.js
+++ bot.js
@@ -64,3 +64,15 @@ const rest = new REST().setToken(token);
 })();
 
 client.login(token);
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
